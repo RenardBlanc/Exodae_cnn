39323f21672496273462102ad314319541450c9e
RenardBlanc <baktacheilyas@gmail.com>
2023-02-27 11:52:37 -0500

    rajout de log sur le fichier bash

--- hyper_param.sh
+++ hyper_param.sh
@@ -9,9 +9,9 @@ if [ -f "$python_file" ]; then
   for Re in 50000 100000 200000 500000 1000000
   do
     # Lancer le fichier Python
+    echo "On lance l'experience pour Re = $Re"
     nohup python3 "$python_file" 0 $Re &
   done
-
 else
   # Afficher un message d'erreur si le fichier n'existe pas
   echo "Le fichier $python_file n'existe pas"
