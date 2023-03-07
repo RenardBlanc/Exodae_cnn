b682c5987f8d0abda4b9f7c4e2caa86afaa8fa3a
RenardBlanc <baktacheilyas@gmail.com>
2023-03-07 14:46:43 -0500

    test du fichier bash

--- hyper_param.sh
+++ hyper_param.sh
@@ -4,19 +4,5 @@ module load cuda cudnn
 # Activation du virtualenv
 source env/bin/activate
 
-# Spécifiez le nom du fichier Python que vous souhaitez lancer
-python_file=experience/exp_class.py
+python3 experience/exp_class.py 0 50000
 
-
-# Vérifiez si le fichier existe
-if [ -f "$python_file" ]; then
-  for Re in 50000 100000 200000 500000 1000000
-  do
-    # Lancer le fichier Python
-    echo "On lance l'experience pour Re = $Re"
-    python3 "$python_file" 0 $Re
-  done
-else
-  # Afficher un message d'erreur si le fichier n'existe pas
-  echo "Le fichier $python_file n'existe pas"
-fi
