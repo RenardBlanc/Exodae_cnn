353ac9b687c565008cf151c5c2c139bd58072bff
RenardBlanc <baktacheilyas@gmail.com>
2023-04-20 09:34:50 -0400

    correction du pb de memoire sur alliance

--- hyper_param.sh
+++ hyper_param.sh
@@ -3,7 +3,7 @@
 #SBATCH --gpus-per-node=p100l:4
 #SBATCH --ntasks-per-node=24
 #SBATCH --exclusive
-#SBATCH --mem=256T
+#SBATCH --mem=40000M
 #SBATCH --time=3:00
 #SBATCH --account=def-fellouah
 #SBATCH --mail-user=baktacheilyas@gmail.com 
