3d8cbce9214b3915dadab873afde404928aa6643
RenardBlanc <baktacheilyas@gmail.com>
2023-04-20 09:28:14 -0400

    correction du pb de memoire sur alliance

--- hyper_param.sh
+++ hyper_param.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --gpus-per-node=p100l:4
+#SBATCH --ntasks-per-node=24
+#SBATCH --exclusive
+#SBATCH --mem=256G
+#SBATCH --time=3:00
+#SBATCH --account=def-fellouah
+#SBATCH --mail-user=baktacheilyas@gmail.com 
 
 module load cuda cudnn
 # Activation du virtualenv
